9ff1b67cd30f4bc6b1182c3dcc9a19fbb2f7230d
Nguyen Tuan Duong <tuanduong18001@gmail.com>
2025-08-20 12:56:39 +0800

    Modularization the code in Barry:main
    There are many lines for differect jobs in the same main function.
    
    Let's divide the code into "greetings", "startSession" and "endSession"
    functions, each has separate mission.

--- src/main/java/Barry.java
+++ src/main/java/Barry.java
@@ -1,12 +1,24 @@
 public class Barry {
     public static void main(String[] args) {
+        greetings();
+        startSession();
+        endSession();
+    }
+
+    public static void greetings() {
         String name = "Barry";
         System.out.println("_".repeat(50));
         System.out.println("Hello I'm " + name);
         System.out.println("What can I do for you?");
         System.out.println("_".repeat(50));
+    }
+
+    public static void startSession() {
+
+    }
+
+    public static void endSession() {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("_".repeat(50));
-
     }
 }
