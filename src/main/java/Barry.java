74d0e30c7ab63a0e73cab369ff80b1eb3d1cc363
Nguyen Tuan Duong <tuanduong18001@gmail.com>
2025-08-20 13:51:29 +0800

    Add support for list
    Add a new private static field to store, change and retrieve the data
    based on user's session.
    Add a new private static method to print all elements in the list.

--- src/main/java/Barry.java
+++ src/main/java/Barry.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Barry {
+    private static ArrayList<String> todosList = new ArrayList<>();
     public static void main(String[] args) {
         greetings();
         startSession();
@@ -19,14 +21,28 @@ public class Barry {
         Scanner scan = new Scanner(System.in);
         while(true) {
             String temp = scan.nextLine();
-            if(temp.equals("bye")) {
+            if (temp.equals("bye")) {
                 System.out.println("    " + "_".repeat(50));
                 break;
+            } else if (temp.equals("list")) {
+                printList();
+                continue;
             }
             System.out.println("    " + "_".repeat(50));
-            System.out.println("    " + temp);
+            todosList.add(temp);
+            System.out.println("    " + "added: " + temp);
             System.out.println("    " + "_".repeat(50));
         }
+    }
+
+    public static void printList() {
+        System.out.println("    " + "_".repeat(50));
+        int i = 1;
+        for (String item : todosList) {
+            System.out.println("    " + i + ". " + item);
+            i++;
+        }
+        System.out.println("    " + "_".repeat(50));
     }
 
     public static void endSession() {
