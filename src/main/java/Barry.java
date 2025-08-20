a98cf168448ab751b48900b59575b52eee000419
Nguyen Tuan Duong <tuanduong18001@gmail.com>
2025-08-20 12:46:16 +0800

    Initialize chatbot: name it
    Chatbot initial name is universal: "Duke".
    
    Rename chatbot to "Barry", rename the file and the class
    correspondingly.Add 1 question message at the beginning and 1 "Goodbye"
    message before shutting down.

---
+++ src/main/java/Barry.java
@@ -0,0 +1,12 @@
+public class Barry {
+    public static void main(String[] args) {
+        String name = "Barry";
+        System.out.println("_".repeat(50));
+        System.out.println("Hello I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("_".repeat(50));
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_".repeat(50));
+
+    }
+}
