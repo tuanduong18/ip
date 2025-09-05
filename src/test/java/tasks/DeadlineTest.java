7472807b397af56dc51984d8281ac8f7de7cf395
Nguyen Tuan Duong <tuanduong18001@gmail.com>
2025-09-05 22:09:39 +0800

    Add more Junit tests

--- src/test/java/tasks/DeadlineTest.java
+++ src/test/java/tasks/DeadlineTest.java
@@ -1,29 +1,29 @@
 package tasks;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class DeadlineTest {
-	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("h:mm a d MMM, yyyy");
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("h:mm a d MMM, yyyy");
 
-	@Test
-	void create_deadline() {
-		LocalDateTime due = LocalDateTime.now();
-		Deadline actual = new Deadline("CS2103T", due);
-		String expected = String.format("[D][ ] CS2103T (by: %s)", formatter.format(due));
-		assertEquals(actual.toString(), expected);
-	}
+    @Test
+    void create_deadline() {
+        LocalDateTime due = LocalDateTime.now();
+        Deadline actual = new Deadline("CS2103T", due);
+        String expected = String.format("[D][ ] CS2103T (by: %s)", formatter.format(due));
+        assertEquals(actual.toString(), expected);
+    }
 
-	@Test
-	void mark_deadline() {
-		LocalDateTime due = LocalDateTime.now();
-		Deadline actual = new Deadline("CS2103T", due);
-		actual.setStatus(true);
-		String expected = String.format("[D][X] CS2103T (by: %s)", formatter.format(due));
-		assertEquals(actual.toString(), expected);
-	}
+    @Test
+    void mark_deadline() {
+        LocalDateTime due = LocalDateTime.now();
+        Deadline actual = new Deadline("CS2103T", due);
+        actual.setStatus(true);
+        String expected = String.format("[D][X] CS2103T (by: %s)", formatter.format(due));
+        assertEquals(actual.toString(), expected);
+    }
 }
